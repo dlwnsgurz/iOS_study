da2d48a148de48d971ae58ff7c8658482d2543b0
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-02-01 21:24:33 +0900

    화면전환 by Multi Unwind Segue

---
+++ Scene-MultiUnwind/Scene-MultiUnwind/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by LEE on 2023/02/01.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goToPage2(_ sender: UIStoryboardSegue){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Scene-MultiUnwind/Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by LEE on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func goToPage1(_ sender: UIStoryboardSegue){
+        
+    }
+
+}
+
