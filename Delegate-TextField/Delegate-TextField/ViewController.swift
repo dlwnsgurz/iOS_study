6af1ed6453db2ef9c5b1356d66f839749d9ddd66
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-02-07 23:02:18 +0900

    델리게이트 패턴 about 텍스트 필드 + 주석 추가

--- Delegate-TextField/Delegate-TextField/ViewController.swift
+++ Delegate-TextField/Delegate-TextField/ViewController.swift
@@ -35,15 +35,18 @@ class ViewController: UIViewController, UITextFieldDelegate{
     }
 
     @IBAction func confirm(_ sender: Any) {
+        // 최초 응답자 상태 해제.
         tf.resignFirstResponder()
         
     }
     
     @IBAction func input(_ sender: Any) {
+        // 최초 응답자 설정.
         tf.becomeFirstResponder()
         
     }
     
+    // 텍스트 필드의 편집이 시작될 때 호출.
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         print("텍스트 필드의 편집이 시작됩니다.")
         
@@ -56,31 +59,40 @@ class ViewController: UIViewController, UITextFieldDelegate{
         }
     }
     
+    // 텍스트 필드의 편집이 시작된 후 호출.
     func textFieldDidBeginEditing(_ textField: UITextField) {
         print("텍스트 필드의 편집이 시작되었습니다.")
     }
     
+    // 텍스트 필드의 내용이 삭제되기 시작할 때 호출.
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
         print("텍스트 필드의 내용이 삭제됩니다.")
         return true
     }
     
+    // 텍스트 필드의 내용이 변경될 때 호출.
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
         print("텍스트 필드의 내용이 \(string)으로 변경되었습니다.")
         return true
     }
     
+    // 텍스트 필드의 엔터 키가 눌리기 전 호출.
+    // return 값에 따라 엔터가 눌릴지 안눌릴지 설정됨.
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print("엔터 키가 눌렸습니다.")
         return true
     }
     
+    
+    // 텍스트 필드의 편집이 종료될 때 호출.
+    // return 값에 따라 편집이 가능할지 불가능할지 설정됨.
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         print("텍스트 필드의 편집이 종료됩니다.")
         return true
     }
     
+    // 텍스트 필드의 편집이 종료되고 호출.
     func textFieldDidEndEditing(_ textField: UITextField) {
         print("텍스트 필드의 편집이 종료되었습니다.")
     }
