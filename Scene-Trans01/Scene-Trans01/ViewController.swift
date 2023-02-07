ed19f7e6f612c332501ff31a8f3ab13552193c0d
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-02-07 18:16:16 +0900

    뷰 컨트롤러 화면 전환 + 주석 작성

--- Scene-Trans01/Scene-Trans01/ViewController.swift
+++ Scene-Trans01/Scene-Trans01/ViewController.swift
@@ -18,10 +18,14 @@ class ViewController: UIViewController {
 
     
     @IBAction func moveNxt(_ sender: Any) {
-        let uvc = self.storyboard!.instantiateViewController(withIdentifier: "secondVC")
         
+        // 이동할 뷰 컨트롤러 객체를 StoryBoardID 정보를 이용하여 참조.
+        guard let uvc = self.storyboard!.instantiateViewController(withIdentifier: "secondVC") else { return }
+        
+        // 화면 전환시 애니메이션 타입.
         uvc.modalTransitionStyle = .coverVertical
         
+        // 인자값으로 뷰 컨트롤러의 인스턴스를 넣고, 프레젠트 메소드 호출
         present(uvc, animated: true)
     }
     
