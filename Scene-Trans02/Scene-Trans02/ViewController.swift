1736a2064f9ceefa6bba86f8c1ad0762d5302afb
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-02-07 18:25:32 +0900

    네비게이션 컨트롤러 화면 전환 + 주석 작성

--- Scene-Trans02/Scene-Trans02/ViewController.swift
+++ Scene-Trans02/Scene-Trans02/ViewController.swift
@@ -16,15 +16,19 @@ class ViewController: UIViewController {
 
     @IBAction func moveByNavi(_ sender: Any) {
         
+        // 이동할 뷰 컨트롤러 인스턴스를 가져온다.
         guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "secondVC") else { return }
         
+        // 네비게이션 컨트롤러를 이용한 화면 전환.
         self.navigationController?.pushViewController(uvc, animated: true)
     }
     
     @IBAction func movePresent(_ sender: Any) {
         
+        // 이동할 뷰 컨트롤러 인스턴스를 가져온다.
         guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "secondVC") else { return }
         
+        // 프레젠테이션 방식으로 화면 전환.
         self.present(uvc, animated: true)
     }
 }
