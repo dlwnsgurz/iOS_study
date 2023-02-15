a289ea49ca95bf39becc32cc3fa71afd69e98700
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-02-15 23:08:15 +0900

    UserInfoManager 객체 구성

--- MyMemory/MyMemory/ProfileVC.swift
+++ MyMemory/MyMemory/ProfileVC.swift
@@ -10,6 +10,7 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     let profileImage = UIImageView()
     let tv = UITableView()
+    let uInfo = UserInfoManager()
     
     override func viewDidLoad() {
         navigationItem.title = "프로필"
@@ -17,7 +18,7 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
         let backBtn = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(close(_:)))
         navigationItem.leftBarButtonItem = backBtn
         
-        let image = UIImage(named: "account.jpg")
+        let image = uInfo.profile
         profileImage.image = image
         profileImage.frame.size = CGSize(width: 100, height: 100)
         
@@ -62,10 +63,10 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
         switch indexPath.row{
             case 0 :
                 cell.textLabel?.text = "이름"
-                cell.detailTextLabel?.text = "꼼꼼한재은 씨"
+                cell.detailTextLabel?.text = uInfo.name ?? "Login please"
             case 1 :
                 cell.textLabel?.text = "계정"
-                cell.detailTextLabel?.text = "ljhlmkljhlmkljhlmk@naver.com"
+                cell.detailTextLabel?.text = uInfo.account ?? "Login please"
             default:
                 break
 
@@ -73,10 +74,62 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if self.uInfo.isLogin == false{
+            self.doLogin(self.tv)
+        }
+        
+    }
+    
     @objc func close(_ sender: Any){
         self.presentingViewController?.dismiss(animated: true)
         
     }
     
+    @objc func doLogin(_ sender: Any){
+        let loginAlert = UIAlertController(title: "LOGIN", message: nil, preferredStyle: .alert)
+        
+        loginAlert.addTextField{
+            $0.placeholder = "Your Account"
+        }
+        
+        loginAlert.addTextField{
+            $0.placeholder = "PassWord"
+            $0.isSecureTextEntry = true
+        }
+        
+        loginAlert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        loginAlert.addAction(UIAlertAction(title: "로그인", style: .destructive){
+            (_) in
+            let account = loginAlert.textFields?[0].text ?? ""
+            let passwd = loginAlert.textFields?[1].text ?? ""
+            
+            if self.uInfo.login(account: account, passwd: passwd){
+                self.tv.reloadData()
+                self.profileImage.image = self.uInfo.profile
+                
+            }else{
+                let msg = "로그인에 실패했습니다."
+                let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+                self.present(alert,animated: true)
+            }
+        })
+        present(loginAlert, animated: true)
+    }
+    
+    @objc func doLogout(_ sender: Any){
+        let msg = "로그아웃 하시겠습니까?"
+        let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        alert.addAction(UIAlertAction(title: "확인", style: .default){
+            (_) in
+            if self.uInfo.logount(){
+                
+            }
+        })
+        present(alert, animated: true)
+    }
     
 }
