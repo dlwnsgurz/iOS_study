c0a0b1fa3aa9e140a922a86f6ee7631d39538a61
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-02-18 00:37:03 +0900

    부서 목록 테이블 구현

--- Chapter06-HR/Chapter06-HR/DepartmentListVC.swift
+++ Chapter06-HR/Chapter06-HR/DepartmentListVC.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class DepartmentListVC: UITableViewController {
 
+    var departList : [(departCd: Int, departTitle: String, departAddr: String)]!
+    let departDAO = DepartmentDAO()
+    
+    func initUI(){
+        let navTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
+        navTitle.numberOfLines = 2
+        navTitle.textAlignment = .center
+        navTitle.font = UIFont.systemFont(ofSize: 14)
+        navTitle.text = "부서 목록 \n" + "총 \(self.departList.count)개"
+        
+        self.navigationItem.titleView = navTitle
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        self.tableView.allowsSelectionDuringEditing = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.departList = self.departDAO.find()
+        self.initUI()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -28,19 +46,66 @@ class DepartmentListVC: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.departList.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        
+        let rowData = self.departList[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DEPART_CELL")
+    
+        cell?.textLabel?.text = rowData.departTitle
+        cell?.textLabel?.font = .systemFont(ofSize: 14)
+        
+        cell?.detailTextLabel?.text = rowData.departAddr
+        cell?.detailTextLabel?.font = .systemFont(ofSize: 12)
+        
+        return cell!
     }
-    */
-
+    
+    @IBAction func add(_ sender: Any) {
+        let alert = UIAlertController(title: "신규 부서 등록", message: "신규 부서를 등록해 주세요", preferredStyle: .alert)
+        
+        alert.addTextField() { $0.placeholder = "부서명" }
+        alert.addTextField() { $0.placeholder = "주소" }
+        
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        alert.addAction(UIAlertAction(title: "확인", style: .default){
+             (_) in
+            let title = alert.textFields?[0].text
+            let addr = alert.textFields?[1].text
+            
+            if self.departDAO.create(title: title!, addr: addr!){
+                self.departList = self.departDAO.find()
+                self.tableView.reloadData()
+                
+                let navTitle = self.navigationItem.titleView as! UILabel
+                navTitle.text = "부서 목록 \n" + "총 \(self.departList.count) 개"
+            }
+            
+        })
+        
+        self.present(alert, animated: true)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let departCd = self.departList[indexPath.row].departCd
+        
+        if departDAO.remove(departCd: departCd){
+            self.departList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
