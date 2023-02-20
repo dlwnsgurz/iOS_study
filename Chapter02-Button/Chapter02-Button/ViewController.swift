997885669f5fe5e51a3c3c2d2c3c50c40ca2f279
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-02-20 20:43:19 +0900

    UIButton 주석 작성

--- Chapter02-Button/Chapter02-Button/ViewController.swift
+++ Chapter02-Button/Chapter02-Button/ViewController.swift
@@ -13,12 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // 버튼 객체 생성
         let btn = UIButton(type: .system)
         btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30)
         btn.center = CGPoint(x: view.frame.width / 2, y: 100)
         btn.setTitle("테스트 버튼", for: .normal)
         
+        // 뷰의 자식 뷰에 추가
         view.addSubview(btn)
+        
+        // 클릭 시 이벤트 연결
         btn.addTarget(self, action: #selector(btnOnClick(_:)), for: .touchUpInside)
         
     }
