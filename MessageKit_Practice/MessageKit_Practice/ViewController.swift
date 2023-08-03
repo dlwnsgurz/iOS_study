e1ca966c7d652a8ada3aa9065ff28c829d3cd26e
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-08-03 16:19:43 +0900

    messageKit 추가

---
+++ MessageKit_Practice/MessageKit_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageKit_Practice
+//
+//  Created by LEE on 2023/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScrollView_Practice/ScrollView_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollView_Practice
+//
+//  Created by LEE on 2023/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
