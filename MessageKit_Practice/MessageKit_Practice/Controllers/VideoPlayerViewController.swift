9ab64aaecfbe3200630b142d6c4f48192120595e
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-08-03 16:40:14 +0900

    Controllers 클래스 생성

---
+++ MessageKit_Practice/MessageKit_Practice/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoViewerViewController.swift
+//  MessageKit_Practice
+//
+//  Created by LEE on 2023/08/03.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
