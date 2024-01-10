a769fa2dd922c8fbc327e6c1e21bc547ad85bed5
LEE <ljhlmkljhlmkljhlmk@naver.com>
2024-01-10 17:50:36 +0900

    1장 CH1

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -4,19 +4,21 @@
 //
 //  Created by LEE on 2023/01/30.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet var uiTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func sayHello(_ sender: Any) {
-        uiTitle.text = "Hello, World"
+    
+    
+    @IBAction func sayHello(_ sender: UIButton) {
+        self.uiTitle.text = "Hello, World!"
     }
-
+    
 }
 
