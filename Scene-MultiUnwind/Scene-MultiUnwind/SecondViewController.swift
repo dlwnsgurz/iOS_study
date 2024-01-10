8b00bda792e1002a561b41a9b40a7f6a504fe9ac
LEE <ljhlmkljhlmkljhlmk@naver.com>
2024-01-10 19:25:05 +0900

    1장CH4 MultiUnwindSegue

--- Scene-MultiUnwind/Scene-MultiUnwind/SecondViewController.swift
+++ Scene-MultiUnwind/Scene-MultiUnwind/SecondViewController.swift
@@ -15,20 +15,9 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func goToPage2(_ sender: UIStoryboardSegue){
-        
-        // 현재 뷰 컨트롤러로 이동.
-        // 하위 뷰 컨트롤러의 인스턴스가 모두 해제됨.
+    @IBAction func goToPage2(_ segue: UIStoryboardSegue){
+        print("gotoPage2")
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Scene-MultiUnwind/Scene-MultiUnwind/ViewController.swift
+++ Scene-MultiUnwind/Scene-MultiUnwind/ViewController.swift
@@ -14,10 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    
-    @IBAction func goToPage1(_ sender: UIStoryboardSegue){
-        // 현재 뷰 컨트롤러로 이동.
-        // 하위 뷰 컨트롤러의 인스턴스가 모두 해제됨.
+    @IBAction func goToPage1(_ segue: UIStoryboardSegue){
+        print("ㅇㅇ")
     }
 
 }
