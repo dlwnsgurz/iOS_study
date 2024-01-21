a6cb9fd1ad2cac55ff57b47bfcfa0a2a148045b3
LEE <ljhlmkljhlmkljhlmk@naver.com>
2024-01-21 20:58:17 +0900

    1장CH10 테이블 뷰

--- MyMovieChart-SpinOff/MyMovieChart-SpinOff/ViewController.swift
+++ MyMovieChart-SpinOff/MyMovieChart-SpinOff/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
 
 }
 
+// 
 extension ViewController: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
